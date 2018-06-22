7d14ca746ec27212f1ffce9ef7d70b5fe3b40dec
Marjorie <marjorieysaito@gmail.com>
2018-06-22 16:25:25 -0300

    alterado nome do pacote

--- src/entity/RaceTrait.java
+++ src/entity/RaceTrait.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Entities.RaceFolder;
+package entity.RaceFolder;
 
 import java.util.List;
 
