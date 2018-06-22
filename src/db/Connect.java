609b4ee37efc80e7f7eb50080a96967dd723a46b
Marjorie <marjorieysaito@gmail.com>
2018-06-22 16:17:50 -0300

    adicionado o form de login e a classe de conexao

---
+++ src/db/Connect.java
@@ -0,0 +1,44 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Marjorie
+ */
+public class Connect {
+    
+    public Connect() {
+        
+    }
+    
+    // Função para conectar ao banco de dados
+    public Connection conectaBD() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String strUrl = "jdbc:mysql://localhost:3306/lcp_dnd?useTimezone=true&serverTimezone=UTC";
+            
+            Connection conn = DriverManager.getConnection(strUrl, "root", "vertrigo");
+            System.out.println("Conexão efetuada com sucesso!");
+            
+            return conn;
+        } catch(Exception e) {
+            System.err.println("Erro ao realizar conexão.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    // Desconectar o banco de dados
+    public void desconectaBD(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+            System.out.println("Conexão finalizada.");
+        } catch(Exception e) {
+            System.err.println("Problemas ao desconectar!");
+        }
+    }
+}
