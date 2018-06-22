84a513e189104f0a0e24f6e0cc8cf747fe3596f8
lukkadd <lukkadd@yahoo.com.br>
2018-06-22 14:19:26 -0300

    added major item improvements

---
+++ src/Entities/Attack.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+/**
+ *
+ * @author lukka
+ */
+public class Attack {
+    //remeber to set add the right bonus when generating the final pdf
+    int dice_count;
+    int dice_type;
+    String damage_type;
+}
---
+++ src/Entities/Monster.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.util.List;
+
+/**
+ *
+ * @author lukka
+ */
+public class Monster {
+    String name;
+    String type;
+    String size;
+    String alignment;
+    String description;
+    float challengeRating;
+    List<String> special_traits;
+    List<String> actions;
+    List<String> reactions;
+    List<String> legendaryActions;
+    int armor_class;
+    String armor_class_type;
+    
+    
+}
